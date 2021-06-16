3f1bfc65820863a52d74d1573d0b8251372bd8af
micheljung <micheljung@users.noreply.github.com>
2021-06-16 16:36:31 +0000

    deploy: d19dad1042e7de88a211627784a1fa618d190f86

---
+++ _next/static/chunks/pages/[id]-994d6365cd1504083d33.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[6],{"8qr+":function(_,n,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[id]",function(){return w("MKm5")}])}},[["8qr+",0,2,1,3,4]]]);
