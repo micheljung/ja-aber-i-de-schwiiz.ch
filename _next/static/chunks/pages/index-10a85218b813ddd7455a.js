36d09d324c1495c70fc36bcff99167c6fc4c980c
micheljung <micheljung@users.noreply.github.com>
2021-06-21 17:37:36 +0000

    deploy: 9a87402db37163cb0a0cb71631bef953384c6532

---
+++ _next/static/chunks/pages/index-10a85218b813ddd7455a.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[11],{RXBc:function(n,t,u){"use strict";u.r(t),u.d(t,"default",(function(){return i}));var _=u("nKUr"),c=(u("q1tI"),u("MKm5"));function i(){return Object(_.jsx)(c.default,{id:"1"})}},vlRD:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u("RXBc")}])}},[["vlRD",0,2,1,3,4]]]);
